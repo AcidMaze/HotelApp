4b16d4c7cda147a80080bb48092f6939d8dc440d
AcidMaze <acidmaze404@gmail.com>
2022-06-06 13:32:03 +0300

    Добавьте файлы проекта.

---
+++ DBMySQLUtils.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MySql.Conn
+{
+    class DBMySQLUtils
+    {
+        public static MySqlConnection GetDBConnection(string host, int port, string database, string username, string password)
+        {
+
+            string connectionString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+
+        }
+    }
+}
+
---
+++ DBUtils.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MySql.Conn
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            return DBMySQLUtils.GetDBConnection("localhost", 3306, "hotel_db", "root", "");
+        }
+    }
+}
